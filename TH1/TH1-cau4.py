d5bdf4bda8195258adf158e9e1754358a19b6f67
Minh Hiểu Trần <minhhieu31012004@gmail.com>
2025-02-11 21:14:01 +0700

    edit TH1/TH1-cau4.py

--- TH1/TH1-cau4.py
+++ TH1/TH1-cau4.py
@@ -11,7 +11,7 @@ def Num2Char(n):
 def xgcd(a, m):
     temp = m
     x0, x1, y0, y1 = 1, 0, 0, 1
-    while m!=0:
+    while m != 0:
         q, a, m = a // m, m, a % m
         x0, x1 = x1, x0 - q * x1
         y0, y1 = y1, y0 - q * y1
@@ -26,18 +26,17 @@ def decryptAF(txt, a, b, m):
         r = r + Num2Char(e)
     return r
 
-def ktntcn(x, y):
-    x, y = min(x, y), max(x, y)
-    for i in range(2, x):
-        if x % i == 0 and y % i == 0:
-            return False
-    return True
+def gcd(a, m):
+    while m != 0:
+        a, m = m, a % m
+    return a
+
 
 cipher_txt = "LOLYLTQOLTHDZTDC"
 hint = "LAMUOI"
 
 for a in range(1, 26):
-    if ktntcn(a, 26):
+    if gcd(a, 26) == 1:
         for b in range(26):
             res = decryptAF(cipher_txt, a, b, 26)
             if hint in res:
